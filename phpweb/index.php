c6180aadca908b58a14c376cc764504d0faa4b34
Scott Baker <scott@perturb.org>
2025-05-22 13:55:16 -0700

    Move the index stats into a function so we can cache it
    This was the slowest page on our site because it hits the DB so many
    times. This should speed things by caching our frequently used DB hits

--- phpweb/index.php
+++ phpweb/index.php
@@ -8,19 +8,11 @@ $json  = $_GET['json']  ?? 0;
 
 ////////////////////////////////////////////////////////
 
-$top_x = get_top_x(10, time() - 3600);
-$s->assign("last_hour", $top_x);
+$stats = get_stats();
+$ms    = intval(sw());
 
-$top_x = get_top_x(10, time() - 86400);
-$s->assign("last_day", $top_x);
-
-$ms = intval(sw());
-$s->assign('page_ms'    , $ms);
-$s->assign('count_hour' , get_count(time() - 3600         , time()));
-$s->assign('count_day'  , get_count(time() - 86400        , time()));
-$s->assign('count_week' , get_count(time() - 86400 * 7    , time()));
-$s->assign('count_month', get_count(time() - 86400 * 30.25, time()));
-$s->assign('count_total', get_count(0, time()));
+$s->assign('page_ms', $ms);
+$s->assign('stats'  , $stats);
 
 ///////////////////////////////////
 
@@ -34,6 +26,33 @@ if ($json) {
 print $s->fetch("tpls/index.stpl");
 
 ////////////////////////////////////////////////////////
+
+function get_stats() {
+	global $mc;
+
+	$ckey = 'index_stats';
+	$data = $mc->get($ckey);
+
+	if ($data) {
+		$data['from_cache'] = true;
+		return $data;
+	}
+
+	$ret["last_hour"] = get_top_x(10, time() - 3600);
+	$ret["last_day"]  = get_top_x(10, time() - 86400);
+
+	$ret['count_hour']  = get_count(time() - 3600         , time());
+	$ret['count_day']   = get_count(time() - 86400        , time());
+	$ret['count_week']  = get_count(time() - 86400 * 7    , time());
+	$ret['count_month'] = get_count(time() - 86400 * 30.41, time());
+	$ret['count_total'] = get_count(0, time());
+
+	$ok = $mc->set($ckey, $ret, 180);
+
+	$ret['from_cache'] = true;
+
+	return $ret;
+}
 
 function get_count($start, $end) {
 	global $dbq;
