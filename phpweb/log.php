1adec4d15db52c3960ae9fd6abc4626a54d828a9
Scott Baker <scott@perturb.org>
2025-10-15 13:48:54 -0700

    Allow filtering by grade

--- phpweb/log.php
+++ phpweb/log.php
@@ -12,7 +12,9 @@ if ($count > 10000) { die; }
 
 ////////////////////////////////////////////////////////
 
-$log = get_log($count, $offset);
+$grade = $_GET['grade'] ?? '';
+
+$log = get_log($count, $offset, $grade);
 
 $ms = intval(sw());
 $s->assign('page_ms', $ms);
@@ -30,14 +32,22 @@ print $s->fetch("tpls/log.stpl");
 
 ////////////////////////////////////////////////////////
 
-function get_log($count, $offset) {
+function get_log($count, $offset, $grade) {
 	global $dbq;
+
+	if ($grade) {
+		$grade_str = $dbq->dbh->quote($grade);
+		$filter    = "WHERE grade = $grade_str";
+	} else {
+		$filter = "";
+	}
 
 	$sql = "SELECT distribution_name, grade, EXTRACT(EPOCH FROM test_ts) as unixtime, distribution_version,
 					osname, guid, octet_length(txt_zstd) as test_bytes
 		FROM test
 		INNER JOIN distribution_info USING (distribution_id)
 		LEFT  JOIN test_results USING (guid)
+		$filter
 		ORDER BY test_ts DESC
 		LIMIT ?
 		OFFSET ?;";
