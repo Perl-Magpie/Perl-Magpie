13a5b27fde36ffd3ee32c0acf069ce15d162cfa8
Scott Baker <scott@perturb.org>
2026-03-24 15:25:21 -0700

    Add some more sanity checking to the API

--- phpweb/api/json-rpc/index.php
+++ phpweb/api/json-rpc/index.php
@@ -75,23 +75,48 @@ class dist {
     public function add_test($test_epoch, $test_uuid, $dist_name, $dist_version, $tester_name, $grade, $perl_version, $os_name, $os_version, $test_body) {
 		global $dbq;
 
-		$exists = $this->get_test($test_uuid);
-		if ($exists) {
-			$this->error_out("Test '$test_uuid' already in DB", 19340);
+		if (preg_match("/::/", $dist_name)) {
+			$this->error_out("Distribution names should be Foo-Bar not Foo::Bar", 59821);
 		}
 
-		$test_time_str = date("Y-m-d H:i:s", $test_epoch);
-		$tester_uuid   = get_tester_uuid($tester_name);
-		$dist_name     = preg_replace("/::/", "-", $dist_name);
-		$dist_id       = get_distribution_id($dist_name, $dist_version);
-		$arch_id       = get_arch_id($os_version);
-		$grade         = strtoupper($grade);
+		////////////////////////////////////////////////////////////////////////////////
 
+		$grade         = strtoupper($grade);
 		$allowed_grades = ['PASS', 'FAIL', 'N/A', 'UNKNOWN'];
 		if (!in_array($grade, $allowed_grades)) {
 			$grade_str = join(", ", $allowed_grades);
 			$this->error_out("Invalid grade. Allowed grades: $grade_str", 49190);
 		}
+
+		////////////////////////////////////////////////////////////////////////////////
+
+		$diff = abs(time() - $test_epoch);
+		if ($diff > 86400 * 365 * 2) {
+			$this->error_out("Test date should be within two years of current day ($diff seconds)", 32178);
+		}
+
+		////////////////////////////////////////////////////////////////////////////////
+
+		$os_name       = strtolower($os_name);
+		$allowed_oses = ['bsd', 'darwin', 'linux', 'solaris', 'windows'];
+		if (!in_array($os_name, $allowed_oses)) {
+			$os_str = join(", ", $allowed_oses);
+			$this->error_out("Invalid OS. Allowed OSs: $os_str", 98471);
+		}
+
+		////////////////////////////////////////////////////////////////////////////////
+
+		$exists = $this->get_test($test_uuid);
+		if ($exists) {
+			$this->error_out("Test '$test_uuid' already in DB", 19340);
+		}
+
+		////////////////////////////////////////////////////////////////////////////////
+
+		$test_time_str = date("Y-m-d H:i:s", $test_epoch);
+		$tester_uuid   = get_tester_uuid($tester_name);
+		$dist_id       = get_distribution_id($dist_name, $dist_version);
+		$arch_id       = get_arch_id($os_version);
 
 		$sql = "INSERT INTO test (test_ts, guid, tester, grade, perl_version, osname, arch_id, distribution_id)
 			VALUES (?,?,?,?,?,?,?,?);";
